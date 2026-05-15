312d79795943b280727a92bfcc2f19e8317713b2
Penguin5687 <mcalpinenoah@gmail.com>
2026-05-15 19:08:59 -0400

    First(Frc haha get it) Commit

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.IntakeSubsystem;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class IntakeSubsystem extends RobotBase{
+    //TO-DO ig, ps. ik I'm already failing at this subsystem
+}
